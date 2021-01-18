f258a1efce22b488ec8c8507a5481074b7752999
Lorenzo Tabasso <lorenzo.tabasso96@gmail.com>
2021-01-18 19:36:29 +0100

    ontop mapping working, individuals with simple ID

--- db/dml.sql
+++ db/dml.sql
@@ -1,9 +1,9 @@
 INSERT INTO source (name, url) values
-	('twitter.it','https://www.twitter.it/');
+	('Twitter','https://www.twitter.it/');
 
 insert into evidence (title, pubblication_date, source_id, url) values
-	('Nicola Zingaretti (@nzingaretti) October 5, 2020','2020-10-05','436444a1-6f56-403d-9526-3d5469ba25a1','https://twitter.com/nzingaretti/status/1313218301200400389?ref_src=twsrc%5Etfw'),
-	('Matteo Salvini (@matteosalvinimi) January 13, 2021', '2021-01-13', '436444a1-6f56-403d-9526-3d5469ba25a1', 'https://twitter.com/matteosalvinimi/status/1349377644941479937');
+	('Nicola Zingaretti (@nzingaretti) October 5, 2020','2020-10-05','1','https://twitter.com/nzingaretti/status/1313218301200400389?ref_src=twsrc%5Etfw'),
+	('Matteo Salvini (@matteosalvinimi) January 13, 2021', '2021-01-13', '1', 'https://twitter.com/matteosalvinimi/status/1349377644941479937');
 
 insert into rating (value, comment, associated_media_url, system_comment, system_url) values
 	('TENDENZIALMENTE VERO',
@@ -39,15 +39,15 @@ INSERT INTO review (title, content, pubblication_date, url, language, rating_id,
 	'2020-10-08', 
 	'https://www.lavoce.info/archives/69816/il-governo-ha-davvero-abolito-i-decreti-sicurezza/', 
 	'it',
-	'4638197e-71d5-4b53-8294-98a6e3fb4a0a', -- rating_id
-	'69eb692d-bede-465b-997b-23ebdf13db68'), -- judgment_id
+	'1', -- rating_id
+	'1'), -- judgment_id
 	('No, “genitore 1 e 2” non tornano sui documenti d’identità (non ci sono mai stati)', 
 	'Al governo si preoccupano di cancellare “padre” e “madre” dalla carta di identità dei minori per sostituirli con genitore 1 e 2', 
 	'2021-01-15', 
 	'https://pagellapolitica.it/dichiarazioni/8801/no-genitore-1-e-2-non-tornano-sui-documenti-didentita-non-ci-sono-mai-stati',
 	'it',
-	'5203a00a-d53e-4a7f-8f71-595533d6aaab', -- rating_id
-	'9a54b9b4-8ffa-464c-8617-52e476f59937'); -- judgment_id
+	'2', -- rating_id
+	'8'); -- judgment_id
 
 INSERT INTO agent (name, tipology) values
 	('Massimo Taddei', 'Person'),
@@ -55,7 +55,7 @@ INSERT INTO agent (name, tipology) values
 	('International Fact Checking Organization', 'Fact Checking Authority'),
 	('Donald TrumpBot', 'Software Agent');
 
-INSERT INTO claim (title, content, pubblication_date, url) values
+INSERT INTO claim (title, content, pubblication_date, url, language) values
 	('Tweet di Nicola Zingaretti', 
 	'Approvato ora in Consiglio dei Ministri il decreto immigrazione. I decreti propaganda/Salvini non ci sono più. Vogliamo un’Italia  più umana e sicura. Un’Europa più protagonista', 
 	'2020-10-05', 
@@ -72,5 +72,3 @@ INSERT INTO claim (title, content, pubblication_date, url) values
 INSERT INTO external_entity (label) values
 	('http://dbpedia.org/page/Matteo_Salvini'),
 	('http://dbpedia.org/page/Nicola_Zingaretti');
-
--- From here to on ou have to link the tables by hand using the UUID! -------------------------------------------------
