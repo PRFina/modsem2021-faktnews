a9930752dbcdca7bd10a04c304abe7d50f0a3ca7
PRFina <pioraffaelefina@outlook.it>
2021-01-22 18:33:13 +0100

    Refactor data cleaning pipeline from notebook into script

---
+++ data/etl_pipeline.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from pathlib import Path
+import numpy as np
+import dateparser
+import lorem
+from sqlalchemy import create_engine
+from psycopg2.extensions import register_adapter, AsIs
+
